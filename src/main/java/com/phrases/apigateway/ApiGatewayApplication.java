ea6d837e7cdb5e119848bf50bece390d5647175a
nicolas <nicolas@wedev.cl>
2019-05-21 23:21:39 -0400

    Set up for api gateway Zuul

---
+++ src/main/java/com/phrases/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.phrases.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import com.phrases.apigateway.filters.PreFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+}
